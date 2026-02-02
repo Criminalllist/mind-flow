97c2432bce0aca864828cadb7d8c37bb8c3ecdf5
Vyacheslav Razlada <vrazlada@mail.ru>
2026-02-02 18:23:37 +0300

    feat: added ts ht

---
+++ src/common/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button type="button" class="button">
+    <slot />
+    <slot name="icon" />
+  </button>
+</template>
+
+<style scoped style="scss">
+.button {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  height: 40px;
+  padding: 10px;
+  background-color: var(--background-button);
+  border: none;
+  border-radius: var(--border-radius-m);
+  font-size: 1.5rem;
+  font-weight: 500;
+  line-height: 1;
+  color: var(--color-white);
+  transition: background-color .3s ease;
+
+  @media (hover: hover) {
+    &:hover {
+      background-color: var(--background-button-hover);
+    }
+  }
+
+  @media (hover: none) {
+    &:active {
+      background-color: var(--background-button-hover);
+    }
+  }
+}
+</style>
---
+++ src/components/UserProfile/UserProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__avatar">
+      <img src="/user-avatar.png" width="128" height="128" alt="user avatar">
+    </div>
+    <div class="user-profile__greeting">
+      <h2 class="user-profile__greeting-title">
+        Добро пожаловать, {{ name }}!
+      </h2>
+      <p class="user-profile__greeting-subtitle">
+        Как вы сегодня себя чувствуете?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { UserProfile } from '@/interfaces/user-profile.iterface';
+
+const { name = 'Will' } = defineProps<UserProfile>()
+</script>
+
+<style scoped style="scss"></style>
