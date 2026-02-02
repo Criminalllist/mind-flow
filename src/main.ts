d5fd33233dcb4196246cb2b9057016097be9d256
Vyacheslav Razlada <vrazlada@mail.ru>
2026-02-02 18:38:41 +0300

    removed pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import '@/assets/styles/main.scss'
 import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
 import App from './App.vue'
 
-// const pinia = createPinia()
 const app = createApp(App)
 
-// app.use(pinia)
 app.mount('#app')
